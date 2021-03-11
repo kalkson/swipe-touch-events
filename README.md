3b98298b31d1d3a4801fc023728afa9bfc32b50e
Damian Kalka <kalka762@gmail.com>
2021-03-11 20:25:48 +0100

    1.0.0 release

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # swipe-touch-events
+
 Javascript based library that allow use swipe-touch events on DOM
+
+# importing
+
+```javascript
+import 'swipe-touch-events';
+```
+
+or import by hand
+
+# usage
+
+```javascript
+document.addEventListener('swiperight', () => {
+  console.log('swiped right');
+});
+```
+
+If you want to use events on specific DOM element:
+
+```javascript
+element.addEventListener('swipedown', () => {
+  console.log('swiped down');
+});
+```
---
+++ index.js
@@ -0,0 +1,39 @@
+module.exports['default'] = (function (element = document, threshold = 50) {
+  let startingX = 0;
+  let startingY = 0;
+  let movementX = 0;
+  let movementY = 0;
+
+  const updateMovement = (e) => {
+    const { clientX, clientY } = e.touches[0];
+
+    movementX = startingX - clientX;
+    movementY = startingY - clientY;
+  };
+
+  element.addEventListener('touchstart', (e) => {
+    const { clientX, clientY } = e.touches[0];
+
+    startingX = clientX;
+    startingY = clientY;
+
+    document.addEventListener('touchmove', updateMovement);
+  });
+
+  document.addEventListener('touchend', (e) => {
+    document.removeEventListener('touchmove', updateMovement);
+
+    let eventType;
+
+    if (movementX > threshold && movementX >= movementY) eventType = 'swipeleft';
+    else if (movementY > threshold && movementY >= movementX) eventType = 'swipeup';
+    else if (movementX < -threshold && movementX <= movementY) eventType = 'swiperight';
+    else if (movementY < -threshold && movementY <= movementX) eventType = 'swipedown';
+    else return;
+
+    const event = new Event(eventType, { bubbles: true });
+    target.dispatchEvent(event);
+  });
+})();
+
+// swipe();
